9f8ebc4b2c8b2c2d13becf2bd66112306f31f9ae
walsker <walskerw@gmail.com>
2019-10-19 20:01:59 -0400

    Finish landing section of portolfio project page
    Also add some assets that I will need in the future.

--- client/src/pages/portfolio/cuHacking.js
+++ client/src/pages/portfolio/cuHacking.js
@@ -1,26 +1,43 @@
-import React, {useEffect} from 'react';
-import {animated, useSpring} from 'react-spring';
+import React from 'react';
+import {Helmet} from 'react-helmet';
+import {animated} from 'react-spring';
 import {Link} from 'react-router-dom';
-import {disableBodyScroll, enableBodyScroll} from 'body-scroll-lock';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faChevronLeft} from '@fortawesome/free-solid-svg-icons';
 
+import {useFadeIn} from 'hooks';
+import {Pane} from 'components';
 import styles from './project.module.css';
+import Project from './project';
 
-const CuHacking = ({transitionState, color}) => {
-  useEffect(() => {
-    disableBodyScroll();
-    return enableBodyScroll;
-  });
-
-  const slideIn = useSpring({
-    config: {clamp: true, mass: 1, tension: 280, friction: 30},
-    width: (transitionState === 'entering' || transitionState === 'entered') ? '100%' : '0%',
-    from: {width: '0%'}
-  });
-
+const CuHacking = props => {
+  const fadeIn = useFadeIn(4, null, true, {delay: 500});
+  
   return (
-    <animated.div className={styles.project} style={{backgroundColor: color, ...slideIn}}>
-      <Link to='/portfolio' className={styles.back}>Back</Link>
-    </animated.div>
+    <Project color={props.color} animate={props.animate} transitionState={props.transitionState}>
+      <Helmet>
+        <title>Wal's Portfolio | cuHacking</title>
+        <style>{'body {color: var(--white)'}</style>
+      </Helmet>
+      <Pane>
+        <div className={styles.landing}>
+          <div className={styles.feature}><h1 id={styles.portfolioFeature} className='unselectable'>W</h1></div>
+          <div className={styles.name}>
+            <animated.h1 style={fadeIn[0]}>cuHacking</animated.h1>
+            <animated.h3 style={fadeIn[1]} className={styles.subtitle}>
+              <a href='https://cuhacking.com' target='_blank' rel='noopener noreferrer external'>
+                cuhacking.com
+              </a>
+            </animated.h3>
+            <animated.p style={fadeIn[2]}>Director of Development.</animated.p>
+          </div>
+        </div>
+        <Link to={{pathname: '/portfolio', state: {animate: true}}} className={styles.backButton}>
+          <FontAwesomeIcon className={styles.backButton} icon={faChevronLeft} size='lg'/>
+        </Link>
+      </Pane>
+    </Project>
   );
 };
+
 export default CuHacking;
--- client/src/pages/portfolio/portfolio.js
+++ client/src/pages/portfolio/portfolio.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from 'react';
+import React from 'react';
 import {Helmet} from 'react-helmet';
 import {animated, useSpring} from 'react-spring';
 import {Link} from 'react-router-dom';
@@ -7,12 +7,13 @@ import {faChevronLeft} from '@fortawesome/free-solid-svg-icons';
 
 import {useFadeIn} from 'hooks';
 import {Pane} from 'components';
+import {WalLogo} from 'assets';
 import styles from './project.module.css';
 import Project from './project';
 
 const Portfolio = props => {
   const fadeIn = useFadeIn(4, null, true, {delay: 500});
-  
+
   return (
     <Project color={props.color} animate={props.animate} transitionState={props.transitionState}>
       <Helmet>
@@ -21,7 +22,7 @@ const Portfolio = props => {
       </Helmet>
       <Pane>
         <div className={styles.landing}>
-          <div className={styles.feature}><h1 id={styles.portfolioFeature} className='unselectable'>W</h1></div>
+          <div className={styles.feature}><WalLogo id={styles.portfolioFeature}/></div>
           <div className={styles.name}>
             <animated.h1 style={fadeIn[0]}>Portfolio</animated.h1>
             <animated.h3 style={fadeIn[1]} className={styles.subtitle}>You're already here!</animated.h3>
